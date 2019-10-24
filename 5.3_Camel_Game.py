f4c35cd800ec829a7e2c6b0edfa1b70f9300be19
halsteaddan <halsteaddan@urbandaleschools.com>
2019-10-24 13:22:49 -0500

    Camel Game.

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -4,7 +4,7 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 '''
 
-done = False
+Done = False
 print ("Welcome to Fortnite pro simulator. In this game your goal is to qalify for the Fortnite world cup without dying.")
 while not Done:
 
