ad9eab144fc585597f0e47abdc78e825bb4fbdce
halsteaddan <halsteaddan@urbandaleschools.com>
2019-10-24 13:21:20 -0500

    Camel Game.

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -4,7 +4,15 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 '''
 
+print ("Welcome to Fortnite pro simulator. In this game your goal is to qalify for the Fortnite world cup without dying.")
+while not Done:
 
+    print("A to use Bathroom today."
+    print("B to grind fortnite for 4 hours today.")
+    print("C to grind Fortnite for 10 hours today")
+    print("D to sleep today.")
+    print("F to see your current status. (This will NOT use up todays action)")
+    print("G to give up on your goal of becoming a pro Fortnite player, exits game.")
 
 
 
